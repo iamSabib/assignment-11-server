defc219f32ef6388f7bb67d6806eb476da2385d6
Sabib <tanzimrahman1415@gmail.com>
2025-01-27 01:17:23 +0600

    added manage service

--- index.js
+++ index.js
@@ -36,16 +36,17 @@ const verifyToken = (req, res, next) => {
 
 
 //JWT token
-app.post('/jwt', (req, res) => { 
+app.post('/jwt', (req, res) => {
     const user = req.body;
     console.log('User:', user);
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '5h'});
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
 
-    res.cookie('token', accessToken, {httpOnly: true,
+    res.cookie('token', accessToken, {
+        httpOnly: true,
         //set true in production 
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none', //change later maybe
-    }).send({success: true});
+    }).send({ success: true });
 }
 );
 
@@ -61,80 +62,99 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9gttp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    const database = client.db('ConsultHive');
-    const servicesCollection = database.collection('consultservices');
-    const bookingCollection = database.collection('bookings');
+        const database = client.db('ConsultHive');
+        const servicesCollection = database.collection('consultservices');
+        const bookingCollection = database.collection('bookings');
 
-    // get all services
-    app.get('/services', async (req, res) => {
-        try {
-            const result = await servicesCollection.find({}).toArray();
-            res.send(result);
-        } catch (error) {
-            console.error(error);
-        }
-    });
+        // get all services
+        app.get('/services', async (req, res) => {
+            try {
+                const result = await servicesCollection.find({}).toArray();
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
-    //get a service by id
-    app.get('/services/:id', async (req, res) => {
-        id = new ObjectId(req.params.id);
-        try {
-            const result = await servicesCollection.findOne({ _id: id });
-            res.send(result);
-        } catch (error) {
-            console.error(error);
-        }
-    });
+        //get a service by id
+        app.get('/services/:id', async (req, res) => {
+            id = new ObjectId(req.params.id);
+            try {
+                const result = await servicesCollection.findOne({ _id: id });
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
-    // get feature services get max 6
-    app.get('/featureservices', async (req, res) => {
-        try {
-            const result = await servicesCollection.find({}).limit(6).toArray();
-            res.send(result);
-        } catch (error) {
-            console.error(error);
-        }
-    });
+        //get services posted by user
+        app.get('/myservices', verifyToken, async (req, res) => {
 
-    // post a service
-    app.post('/services', async (req, res) => {
-        try {
-            const result = await servicesCollection.insertOne(req.body);
-            res.send(result);
-        } catch (error) {
-            console.error(error);
-        }
+            const email = req.query.email;
+            if (email !== req.user.email) return res.status(403).send({ message: 'Forbidden Access' });
 
-    });
+            try {
+                const result = await servicesCollection.find({ email });
+                res.send(result);
+            } catch (error) {
+                console.error('Error fetching services:', error);
+                res.status(500).send({ message: 'Internal Server Error' });
+            }
 
-    //book a service
-    app.post('/book-services', async (req, res) => {
-        try {
-            const result = await bookingCollection.insertOne(req.body);
-        } catch (error) {
-            console.error(error)
-        }
-    })
+            // res.send({message: "Nice"})
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        })
+
+        // get feature services get max 6
+        app.get('/featureservices', async (req, res) => {
+            try {
+                const result = await servicesCollection.find({}).limit(6).toArray();
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+
+        // post a service
+        app.post('/services', async (req, res) => {
+            try {
+                const result = await servicesCollection.insertOne(req.body);
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+
+        });
+
+        //book a service
+        app.post('/book-services', async (req, res) => {
+            try {
+                const result = await bookingCollection.insertOne(req.body);
+                res.send(result)
+            } catch (error) {
+                console.error(error)
+            }
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -147,9 +167,9 @@ app.get('/sservices', verifyToken, (req, res) => {
     // console.log('cookies', req.cookies);
     const email = req.query.email;
     console.log('Email:', email);
-    if (email !== req.user.email) return res.status(403).send({message: 'Forbidden Access'});
+    if (email !== req.user.email) return res.status(403).send({ message: 'Forbidden Access' });
 
-    res.send({message: 'Services Alright'});
+    res.send({ message: 'Services Alright' });
 });
 
 app.listen(port, () => {
